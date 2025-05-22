da7dd9e5ad5f419ef6d893fe45033e0fccfbcdda
Adolfo <adolfochaveziv@gmail.com>
2025-05-22 18:37:47 -0500

    server.js - contact page message to email system

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const app = express();
 
+require('dotenv').config();
+
 const EMAIL_USER = process.env.EMAIL_USER;
 const EMAIL_PASS = process.env.EMAIL_PASS;
 
@@ -11,10 +13,38 @@ app.use(bodyParser.json());
 app.post('/submit', (req, res) => {
     const { name, email, message } = req.body;
 
-    // 
+    // Nodemailer API for sending emails
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+    
+    // Mail data
+    const mailOptions = {
+        from: email,
+        to: process.env.EMAIL_USER,
+        subject: `Message from ${name}`,
+        text: message
+    };
+    
+    // Error handling & console log
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send('Error sending email');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.send('Email sent!');
+        }
+    });
+
+    // Nodemailer end
 
     res.send('Email sent!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT; // Add || 3000 for local stuff
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
